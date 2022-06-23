c0e2ffc26e3f97bacbeebd1374d0027a97a9fc5d
EvoX7 <creativesoultouch@gmail.com>
2022-06-23 18:13:42 +0200

    Update - added generated squares on button click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-// ! Selecting grid output 
+// ! Selecting grid output (parent)
 const myContainer = document.getElementById("my_container");
 
-// ! Defining how many times i need to generate a square
-    // ? generate a square 
+// * Defining clickable button
+const play = document.getElementById("play");
+
+// ! Creating a clickable PLAY button for user to generate a new grid
+
+// ! Defining how many times i need to generate a square in the grid
+    // ? generate a square (1oo)
     // ? assign proprieties they need 
     // ? add numbers inside the squares
     // ? add the new generated square to the parent (output)
@@ -10,6 +15,26 @@ const myContainer = document.getElementById("my_container");
         // * everytime there is a click, add a class if absent
         // * remove a class if present
         
+
+for (let i = 1; i < 100 + 1; i++){
+    const genSquare = fNewSquare();
+
+    play.addEventListener("click", function(){
+        myContainer.append(genSquare);
+})
+
+}
+
+
+
+function fNewSquare(){
+    const newBox = document.createElement("div");
+    newBox.classList.add("square");
+    return newBox;
+
+    
+
+}
 
 
 
