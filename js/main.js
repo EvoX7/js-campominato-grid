7d7e87f85b0b84be9ebc2433b27c6d819292963c
EvoX7 <creativesoultouch@gmail.com>
2022-06-23 17:34:07 +0200

    Starting with MineSweeper

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// ! Selecting grid output 
+const myContainer = document.getElementById("my_container");
+
+// ! Defining how many times i need to generate a square
+    // ? generate a square 
+    // ? assign proprieties they need 
+    // ? add numbers inside the squares
+    // ? add the new generated square to the parent (output)
+    // ? the squares will be clickable:
+        // * everytime there is a click, add a class if absent
+        // * remove a class if present
+        
+
+
+
+
